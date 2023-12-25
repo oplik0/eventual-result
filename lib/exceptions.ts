0e3949798e6e7c5f9a9a6d998c62cd9317b794f4
Opliko <opliko.reg@protonmail.com>
2023-12-25 18:19:06 +0100

    add color

--- lib/exceptions.ts
+++ lib/exceptions.ts
@@ -9,7 +9,7 @@ class ErrWrappingValue extends Error {
       errorOptions.cause = value;
     }
 
-    super(`${message} ${value.toString()}`, errorOptions);
+    super(`${message}: ${value.toString()}`, errorOptions);
 
     this.value = value;
   }
